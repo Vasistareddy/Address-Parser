a821bf0ac1a1d8be74c9874cde1f9e64d383f45d
vasista <vasista@inzyte.com>
2018-10-13 14:05:00 +0530

    init file added

---
+++ README.md
@@ -0,0 +1,23 @@
+**For now we are parsing US-Addresses only**
+
+# Description
+
+**To parse the US addresses**
+
+**To map the state code(AZ) to state name(Arizona)**
+
+**To skip the other country addresses**
+
+**Some additional keys are added to the output of *usaddress* python module**
+
+# Methods
+	- parse_address
+
+# Params
+	- text
+
+# Function Call
+	- parse_address("455 North 3rd 190 Arizona Center,Phoenix, AZ 85004")
+
+# GET API Call
+	- http://172.16.237.232:9006/usparse?text=455%20North%203rd%20190%20Arizona%20Center,Phoenix,%20AZ%2085040
---
+++ app.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask, jsonify, make_response, request, redirect
+from US_address_parser import parse_address
+
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+
+@app.route('/usparse', methods=['GET', 'POST'])
+def address_api():
+    if request.method == 'GET':
+        text = request.args.get('text')
+        address = parse_address(text)
+        if address:
+            return make_response(jsonify({'Address':address, 'status_code':200}), 200)
+        return make_response(jsonify({'error':'sorry! unable to parse', 'status_code':500}), 500)
+
+if __name__ == '__main__':
+   app.run(host='0.0.0.0', port=9006, debug=False)
