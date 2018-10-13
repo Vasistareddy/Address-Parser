8eba9b6b969e65a6d8620e820a65f8d56bdf1332
vasista <vasista@inzyte.com>
2018-10-13 11:17:14 +0530

    first push

---
+++ US_address_parser.py
@@ -0,0 +1,17 @@
+import usaddress
+import re
+
+states = {'AK': 'Alaska', 'AL': 'Alabama', 'AR': 'Arkansas', 'AS': 'American Samoa', 'AZ': 'Arizona', 'CA': 'California', 'CO': 'Colorado', 'CT': 'Connecticut', 'DC': 'District of Columbia', 'DE': 'Delaware', 'FL': 'Florida', 'GA': 'Georgia', 'GU': 'Guam', 'HI': 'Hawaii', 'IA': 'Iowa', 'ID': 'Idaho', 'IL': 'Illinois', 'IN': 'Indiana', 'KS': 'Kansas', 'KY': 'Kentucky', 'LA': 'Louisiana', 'MA': 'Massachusetts', 'MD': 'Maryland', 'ME': 'Maine', 'MI': 'Michigan', 'MN': 'Minnesota', 'MO': 'Missouri', 'MP': 'Northern Mariana Islands', 'MS': 'Mississippi', 'MT': 'Montana', 'NA': 'National', 'NC': 'North Carolina', 'ND': 'North Dakota', 'NE': 'Nebraska', 'NH': 'New Hampshire', 'NJ': 'New Jersey', 'NM': 'New Mexico', 'NV': 'Nevada', 'NY': 'New York', 'OH': 'Ohio', 'OK': 'Oklahoma', 'OR': 'Oregon', 'PA': 'Pennsylvania', 'PR': 'Puerto Rico', 'RI': 'Rhode Island', 'SC': 'South Carolina', 'SD': 'South Dakota', 'TN': 'Tennessee', 'TX': 'Texas', 'UT': 'Utah', 'VA': 'Virginia', 'VI': 'Virgin Islands', 'VT': 'Vermont', 'WA': 'Washington', 'WI': 'Wisconsin', 'WV': 'West Virginia', 'WY': 'Wyoming'}
+
+def parse_address(address):
+    address_out = {}
+    if address:
+        pincodes = re.findall(r'\b[A-Z]{2}[ ]{0,1}\d{5}\b', address)
+        if pincodes:
+            l = usaddress.parse(address)
+            l.append((address, 'Address'))
+            l2 = [(t[1],t[0].strip(',')) if type(t[0]) is not list else (t[1],t[0]) for t in l]
+            address_out = dict(l2)
+            if address_out['StateName'] and len(address_out['StateName']) == 2:
+                address_out['StateName'] = states[address_out['StateName']]
+    return address_out
