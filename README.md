a3aa6ddc63cfdaaf760a8e3ccf8d89b931a6405c
vasista <vasista@inzyte.com>
2018-10-16 15:05:04 +0530

    canada address support added

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-**WARNING -- For now we are parsing US-Addresses only**
+**WARNING -- For now we are parsing US and CANADA-Addresses only**
 
 # Project Description
 
-**To parse the US addresses**
+**To parse the US and CANADA addresses**
 
 **To map the state code(AZ) to state name(Arizona)**
 
@@ -18,6 +18,8 @@
 ```
 from US_address_parser import parse_address
 parse_address("455 North 3rd 190 Arizona Center,Phoenix, AZ 85004")
+from CANADA_address_parser import parse_address
+parse_address('2620 32nd St. NE Calgary AB T1Y 6B8')
 ```
 
 # Methods
@@ -26,8 +28,14 @@ parse_address("455 North 3rd 190 Arizona Center,Phoenix, AZ 85004")
 # Params
 	- text
 
+# Endpoints
+## /usparse
+## /caparse
+
 # GET API Call
 	- http://localhost:9006/usparse?text=455%20North%203rd%20190%20Arizona%20Center,Phoenix,%20AZ%2085040
+	- http://localhost:9006/caparse?text=2620%2032nd%20St.%20NE%20Calgary%20AB%20T1Y%206B8
 
 # GUnicorn Command to run
 	- nohup /usr/local/bin/gunicorn --name=address_parser --bind=localhost:9006 --log-level=DEBUG --workers=3 --timeout=6000 wsgi:app &
+	- use nohup or make a systemctl daemon service
--- app.py
+++ app.py
@@ -1,18 +1,28 @@
 import os
 from flask import Flask, jsonify, make_response, request, redirect
-from US_address_parser import parse_address
+import US_address_parser
+import canada_address_parser
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 
 @app.route('/usparse', methods=['GET', 'POST'])
-def address_api():
+def us_api():
     if request.method == 'GET':
         text = request.args.get('text')
-        address = parse_address(text)
+        address = US_address_parser.parse_address(text)
+        if address:
+            return make_response(jsonify({'Address':address, 'status_code':200}), 200)
+        return make_response(jsonify({'error':'sorry! unable to parse', 'status_code':500}), 500)
+
+@app.route('/caparse', methods=['GET', 'POST'])
+def ca_api():
+    if request.method == 'GET':
+        text = request.args.get('text')
+        address = canada_address_parser.parse_address(text)
         if address:
             return make_response(jsonify({'Address':address, 'status_code':200}), 200)
         return make_response(jsonify({'error':'sorry! unable to parse', 'status_code':500}), 500)
 
 if __name__ == '__main__':
-   app.run(port=9006, debug=False)
+   app.run(host='0.0.0.0', port=9006, debug=False)
